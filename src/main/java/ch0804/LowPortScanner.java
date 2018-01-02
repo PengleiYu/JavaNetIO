bdbec38a35a7a309bafe426c07778ccf255c8f4a
penglei <yupengniaosanshi@gmail.com>
2018-01-02 15:50:50 +0800

    save

--- src/main/java/ch0804/LowPortScanner.java
+++ src/main/java/ch0804/LowPortScanner.java
@@ -1,10 +1,7 @@
 package ch0804;
 
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.net.UnknownHostException;
+import java.net.*;
 
 /**
  * 查看主机前1024个端口哪个安装有tcp服务器
@@ -19,8 +16,8 @@ public class LowPortScanner {
                 //构造但不连接
                 //似乎很多网络服务器防扫描，不返回tcp响应，所以连接时指定timeout
                 Socket socket = new Socket();
-                SocketAddress address= new InetSocketAddress(host,i);
-                socket.connect(address,1000);
+                SocketAddress address = new InetSocketAddress(host, i);
+                socket.connect(address, 1000);
                 System.out.println("There is a server on port " + i + " of " + host);
                 socket.close();
             } catch (UnknownHostException e) {
