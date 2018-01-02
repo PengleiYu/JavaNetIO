d78980f6107bbe72399ffe109aec1f868cd945d4
penglei <yupengniaosanshi@gmail.com>
2018-01-02 11:16:24 +0800

    测试dict服务器

---
+++ src/main/java/dict/DictClient.java
@@ -0,0 +1,27 @@
+package dict;
+
+import java.io.*;
+import java.net.Socket;
+
+/**
+ * Created by yupenglei on 18/1/2.
+ */
+public class DictClient {
+    public static void main(String[] args) {
+        try (Socket socket = new Socket("dict.org", 2628)) {
+            Writer writer = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
+            writer.write("define fd-eng-lat gold\r\n");
+            writer.flush();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket
+                    .getInputStream(), "UTF-8"));
+            for (String line = reader.readLine(); !line.equals("."); line = reader.readLine()) {
+                System.out.println(line);
+            }
+            writer.write("quit\r\n");
+            writer.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
