fca991b9390bccd948bd42deff416724a0858998
penglei <yupengniaosanshi@gmail.com>
2018-01-02 11:18:01 +0800

    save

--- src/main/java/dict/DictClient.java
+++ src/main/java/dict/DictClient.java
@@ -9,6 +9,7 @@ import java.net.Socket;
 public class DictClient {
     public static void main(String[] args) {
         try (Socket socket = new Socket("dict.org", 2628)) {
+            socket.setSoTimeout(15000);
             Writer writer = new OutputStreamWriter(socket.getOutputStream(), "UTF-8");
             writer.write("define fd-eng-lat gold\r\n");
             writer.flush();
