3df2c2591160b3f4bf9f3ee71a58cf5a1b7a315f
penglei <yupengniaosanshi@gmail.com>
2018-01-03 18:08:26 +0800

    基本的流操作

---
+++ src/main/java/ch0201/BaseStream.java
@@ -0,0 +1,49 @@
+package ch0201;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+/**
+ * 基本的流操作
+ * Created by yupenglei on 18/1/3.
+ */
+public class BaseStream {
+    private static final int firstPrintableCharacter = 33;
+    private static final int numberOfPrintableCharacter = 94;
+    private static final int numberOfCharactersPerLine = 72;
+
+    private static int getPrintableChar(int position) {
+        return ((position - firstPrintableCharacter) % numberOfPrintableCharacter) +
+                firstPrintableCharacter;
+    }
+
+    public static void generateCharacters1(OutputStream out) throws IOException {
+        int start = firstPrintableCharacter;
+        while (true) {
+            for (int i = start; i < start + numberOfCharactersPerLine; i++) {
+                out.write(getPrintableChar(i));
+            }
+            out.write('\r');
+            out.write('\n');
+            start = getPrintableChar(start + 1);
+        }
+    }
+
+    public static void generateCharacters2(OutputStream out) throws IOException {
+        int start = firstPrintableCharacter;
+        byte[] line = new byte[numberOfCharactersPerLine + 2];
+        while (true) {
+            for (int i = start; i < start + numberOfCharactersPerLine; i++) {
+                line[i - start] = (byte) getPrintableChar(i);
+            }
+            line[numberOfCharactersPerLine] = '\r';
+            line[numberOfCharactersPerLine + 1] = '\n';
+            out.write(line);
+            start = getPrintableChar(start + 1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
